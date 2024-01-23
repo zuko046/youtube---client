8e55a7cf3ab55db2c07042b3451e9f25a8feeb90
Arjun Raj <71980597+zuko046@users.noreply.github.com>
2024-01-23 11:27:41 +0530

    material ui and vite react with ts

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import Button from "@mui/material/Button";
+
+function App() {
+  return (
+    <>
+      <div>
+        <h1>hai </h1>
+        <Button variant="contained">Hello world</Button>
+      </div>
+    </>
+  );
+}
+
+export default App;
